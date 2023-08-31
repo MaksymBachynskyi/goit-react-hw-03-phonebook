6e8de5934305d7bd5cceed25dc031d8f91e6adda
MaksymBachynskyi <125116688+MaksymBachynskyi@users.noreply.github.com>
2023-08-31 15:19:46 +0300

    first

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,14 +5,19 @@ import { ContactForm } from '../contactForm/contactForm.jsx';
 import { Container } from './Layout.styled';
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  componentDidMount() {
+    const storage = localStorage.getItem('contacts');
+
+    this.setState({ contacts: JSON.parse(storage) ?? [] });
+  }
+  componentDidUpdate(prevState, nextState) {
+    if (nextState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   onAddNumber = newNumber => {
     const isIcluded = this.state.contacts.some(item => {
       return (
